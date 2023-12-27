41ed8d0fa478e2e267f59d69c9e8dbfbe92864b6
Aayush <guptaayush3108@gmail.com>
2023-12-28 00:56:09 +0530

    Fix Production Require

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
+import commonjs from "vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/QR/",
-  plugins: [react()],
+  base: "/",
+  plugins: [
+    react(),
+    commonjs({
+      filter(id) {
+        if (id.includes("node_modules/qrcode-with-logos")) {
+          return true;
+        }
+      },
+    }),
+  ],
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [esbuildCommonjs(["qrcode-with-logos", "qrcode"])],
+    },
+  },
 });
